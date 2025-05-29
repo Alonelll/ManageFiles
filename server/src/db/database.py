5cd43ee0e303604374a3d9332f7e56dd9563c07a
lennart-falke <l.luttman@outlook.de>
2025-05-29 22:07:30 +0200

    Datenbank Anbindung sollte jetzt funktionieren

--- server/src/db/database.py
+++ server/src/db/database.py
@@ -1,30 +1,15 @@
 import mariadb
 import sys
 
-def connect_db():
-    try:
-        conn = mariadb.connect(
-            user="test",
-            password="Test!1234",
-            host="maradb",      # Docker Service Name
-            port=3306,
-            database="file_management"
-        )
-        print("✅ Verbindung erfolgreich!")
-        return conn
-    except mariadb.Error as e:
-        print(f"❌ Fehler bei Verbindung zu MariaDB: {e}")
-        sys.exit(1)
+HOST = "mariadb-local"
+PORT = 3306
+DATABASE = "file_management"
 
-if __name__ == "__main__":
-    conn = connect_db()
-    cur = conn.cursor()
-    cur.execute("SELECT DATABASE();")
-    db = cur.fetchone()
-    print(f"Verbunden mit DB: {db[0]}")
-
-# Select script um benutzer abzu fragen
-# SELECT user, host, plugin FROM mysql.user WHERE user = 'test';
-
-# Prompt um in den container rein zu gehen
-# docker exec -it mariadb-local mariadb -u root -proot
+def connect_db(user:str, password:str):
+    return mariadb.connect(
+        user=user,
+        password=password,
+        host=HOST,
+        port=PORT,
+        database=DATABASE
+    )
